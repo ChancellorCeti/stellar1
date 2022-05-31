a2e1a90de06eade3312d78055c7f10e6fe91997c
ChancellorCeti <74802589+ChancellorCeti@users.noreply.github.com>
2022-05-31 07:38:26 -0500

    Add files via upload

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import {Text,View,Stylesheet} from react-native;
+export default class Home extends Component{
+    render(){
+    return(
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+            <Text>Home</Text>
+        </View>
+    )
+    };
+}
